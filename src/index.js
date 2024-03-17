5c684ce8c09ab71cd0e52fba56304ccf920e4eea
laumonmay <laumonmay@hotmail.com>
2024-03-17 21:16:41 +0100

    motor de plantillas

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const mysql = require("mysql2/promise");
 const server = express();
 server.use(cors());
 server.use(express.json({ limit: "25mb" }));
+server.set('view engine', 'ejs');
 
 // enlazar base de datos // se crea esta función, que se usará cuando hagamos un endpoint donde queramos hacer una petición.
 async function getConnection() {
@@ -123,15 +124,55 @@ server.post("/api/projectCard", async (req, res) => {
 });
 
 // Mostrar el detalle de un proyecto (serv. dinámicos)
-server.get("/projectCard/:id", (req, res) => {
+server.get("/projectCard/:id", async (req, res) => {
+
   // Recibo el id del proyecto en un URL param
+  const idProjectCard = req.params.id;
+  console.log(req.params);
+
   // 1. Conectar a la bbdd
+  const conn = await getConnection();
+
   // 2. Lanzar un SELECT para recuperar 1 proyecto con el id <- req.params
-  // 3. Hago un template (EJS)
+  const queryGetProjectCard = `
+  SELECT *
+  FROM projects
+  JOIN users 
+  ON projects.fkUsers = users.idusers AND projects.idprojects = ?
+  `
+  const [resultsProjectCard] = await conn.query(queryGetProjectCard, [idProjectCard]);
+
+  // 3. Hago un template, creando el fichero project.ejs
+
   // 4. Cierro la conexión
+  conn.end();
+
   // 5. res.render('plantilla', resultado)
+  res.render('project', resultsProjectCard[0])
 });
 
 // crear servidor de estáticos
 
 server.use(express.static("./public"));
+
+
+// Crea un servidor de estáticos para los estilos en tu servidor:
+
+// Crea el fichero main.css de estilos en la carpeta src del servidor src/public-css/.
+
+// Configura el servidor de estáticos en index.js para que esté disponible el archivo css.
+
+server.use(express.static("./src/public-css"));
+
+// Incluye el fichero main.css en la plantilla, presta mucha atención a la ruta del css. En la plantilla de la carpeta de views, quedaría asi:
+//<link rel=“stylesheet” href=“/main.css” />
+
+
+exports.server = server;
+
+
+
+
+
+
+
