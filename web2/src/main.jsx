b637e6daa9daf73f159b91a307c0f8a8b98067d6
reyesjerez <reyesjerezpe@gmail.com>
2024-03-18 22:15:27 +0100

    fix render4

--- web2/src/main.jsx
+++ web2/src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
